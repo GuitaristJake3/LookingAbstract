276cb079c4c83ac4c137b91e265e40d4999e1562
GuitaristJake3 <47261035+GuitaristJake3@users.noreply.github.com>
2021-11-29 15:56:18 +0000

    Built a test for implementing the abstract class overriding the abstrract method and overloading the other method

---
+++ AbstractTest/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractTest
+{
+    abstract class Bird
+    {
+        /// <summary>
+        /// Bird is an abstract class
+        /// When we want to use it we will need to implement the abstract method birdCall
+        /// But we will be able to use the non abstract method birdNest
+        /// </summary>
+        public abstract void birdCall();        //birdCall is an abstract method which does not yet exist
+        public void birdNest()
+        {
+            Console.WriteLine("zzzzz");
+        }
+    }
+}
--- AbstractTest/Program.cs
+++ AbstractTest/Program.cs
@@ -8,8 +8,16 @@ namespace AbstractTest
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Bird emperor = new Bird();      Bird is abstract so it can't exist
+            Penguin emperor = new Penguin();
+            emperor.birdCall();
+            //This implements polymorphism of birdNest function
+            emperor.birdNest();     //Calls base version
+            emperor.birdNest(10f);      //Calls the overloaded version
+            Console.ReadLine();
         }
     }
 }
